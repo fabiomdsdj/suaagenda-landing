3f80e565b7561662a6c5032fcb16d9241ae9cf03
Fabio Mendes <fabiomdsdj@gmail.com>
2026-05-24 19:23:25 -0300

    modified:   server/api/available-ufs.ts

--- server/api/available-ufs.ts
+++ server/api/available-ufs.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event)
-
+  console.log('[available-ufs] token:', JSON.stringify(config.sitemapInternalToken)) // ← aqui
   const data = await $fetch<{ data: { uf: string; ufSlug: string; count: number }[] }>(
     `${config.public.apiBase}/geoLocations/available-ufs`,
     {
